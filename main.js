55bf662cd611beab488f13bb697f5b971107aef3
psvel332~ <sakthivelpk2002@gmail.com>
2023-02-17 10:40:46 +0530

    new

---
+++ main.js
@@ -0,0 +1,16 @@
+const header =document.querySelector("header");
+
+ window.addEventListener ("scroll", function(){
+    header.classList.toggle ("sticky" ,this.window.scrollY > 0);
+ });
+ let menu =document.querySelector('#menu-icon');
+ let navbar =document.querySelector('.navbar');
+ menu.onclick =() => {
+   menu.classList.toggle('fa-xmark');
+   navbar.classList.toggle('open');
+ };
+ window.onscroll =() => {
+   menu.classList.remove('fa-xmark');
+   navbar.classList.remove('open');
+ }
+ 
